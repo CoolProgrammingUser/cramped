be501a4de0ca72354f6a7060a949ccd2dd61b96e
Robert <ambitiousbusinessowner@gmail.com>
2016-12-16 18:53:23 -0500

    made all of the titles "Cramped"

--- behavior.js
+++ behavior.js
@@ -1,4 +1,8 @@
 window.addEventListener("load", function() {
+    // makes the title of all the pages "Cramped"
+    document.body.insertBefore(document.createElement("title"), document.head.childNodes[0]);
+    document.getElementsByTagName("title")[0].innerHTML = "Cramped";
+    // adds the navigation section to all the pages
     document.body.insertBefore(document.createElement("nav"), document.body.childNodes[0]);
     var file = new XMLHttpRequest();
     file.open("GET", "navigation.html", false);
