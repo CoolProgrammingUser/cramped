2fa7d98e6889c64ce3e6b3b76fb0757a2a25bdc1
Robert <ambitiousbusinessowner@gmail.com>
2016-12-17 23:50:48 -0500

    added a comment

--- behavior.js
+++ behavior.js
@@ -5,7 +5,7 @@ window.addEventListener("load", function() {
     // adds the navigation section to all the pages
     document.body.insertBefore(document.createElement("nav"), document.body.childNodes[0]);
     var file = new XMLHttpRequest();
-    file.open("GET", "navigation.html");
+    file.open("GET", "navigation.html");  // Don't add false as an extra argument (browsers don't like it). (default: asynchronous=true)
     file.onreadystatechange = function () {
         if(file.readyState === 4) {
             if(file.status === 200 || file.status == 0) {
