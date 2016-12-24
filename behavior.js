935fa2f50db46b91cb7989f16263420f9fb7a1eb
Robert <ambitiousbusinessowner@gmail.com>
2016-12-23 21:48:07 -0500

    added pageJump

--- behavior.js
+++ behavior.js
@@ -1,3 +1,26 @@
+function pageJump(ID) {
+    /*
+    makes a section to jump to certain parts of the page
+    */
+    document.getElementsByTagName("h1")[0].id = "top";
+    var division = document.getElementById(ID);
+    var contents = document.createElement("div");
+    contents.innerHTML = "<h2>Jump to:</h2>";
+    var sections = division.getElementsByTagName("h2");
+    var toTop = document.createElement("a");
+    toTop.href = "#top";
+    toTop.innerHTML = "To top";
+    for (var index=0; index<sections.length; index++) {  // I would use (var in array), but index exceeds entries.length for no apparent reason
+        var inside = sections[index].innerHTML.trim();  // The inner HTML has a bunch of whitespace for no apparent reason.
+        sections[index].id = inside;
+        contents.innerHTML += "<p><a href='#" + inside + "'>" + inside + "</a></p>";
+        division.insertBefore(toTop.cloneNode(true), division.getElementsByTagName("h2")[index].nextSibling);  // inserts after <h2>
+        // toTop needs to be cloned so it doesn't keep getting reasigned to the next place (it also needs to have true to clone all children of the node, although it doesn't apply here)
+    }
+    contents.innerHTML += "<br>";
+    document.body.insertBefore(contents, division);  // .insertBefore() only works for the immediate descendants of the parent
+}
+
 window.addEventListener("load", function() {
     // makes the title of all the pages "Cramped"
     document.head.insertBefore(document.createElement("title"), document.head.childNodes[0]);
