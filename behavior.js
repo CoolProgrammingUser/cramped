2150712651ea9c490876f698c95d4e0c8749f13f
Robert <ambitiousbusinessowner@gmail.com>
2017-01-19 19:43:32 -0500

    tried putting the HTML reading into a function

--- behavior.js
+++ behavior.js
@@ -1,3 +1,16 @@
+function read(URL) {
+    var file = new XMLHttpRequest();
+    file.open("GET", URL);  // Don't add false as an extra argument (browsers don't like it). (default: asynchronous=true)
+    file.onreadystatechange = return function () {
+        if(file.readyState === 4) {
+            if(file.status === 200 || file.status == 0) {
+                return file.responseText;
+            }
+        }
+    }
+    file.send();
+}
+
 function pageJump(ID) {
     /*
     makes a section to jump to certain parts of the page
@@ -22,29 +35,24 @@ function pageJump(ID) {
 }
 
 window.addEventListener("load", function() {
+    
     // adds the universal formatting
     var style = document.createElement("link");
     style.rel = "stylesheet";
     style.href = "formatting.css";
     document.head.insertBefore(style, document.head.children[0]);
+    
     // makes the title of all the pages "Cramped"
     document.head.insertBefore(document.createElement("title"), document.head.childNodes[0]);
     document.getElementsByTagName("title")[0].innerHTML = "Cramped";
+    
     // links the favicon
     var icon = document.createElement("link");
     icon.rel = "icon";
     icon.href = "images/favicon.ico";
     document.head.insertBefore(icon, document.head.children[0]);
+    
     // adds the navigation section to all the pages
     document.body.insertBefore(document.createElement("nav"), document.body.childNodes[0]);
-    var file = new XMLHttpRequest();
-    file.open("GET", "navigation.html");  // Don't add false as an extra argument (browsers don't like it). (default: asynchronous=true)
-    file.onreadystatechange = function () {
-        if(file.readyState === 4) {
-            if(file.status === 200 || file.status == 0) {
-                document.getElementsByTagName("nav")[0].innerHTML = file.responseText;
-            }
-        }
-    }
-    file.send();
+    document.getElementsByTagName("nav")[0].innerHTML = read("navigation.html");
 });
