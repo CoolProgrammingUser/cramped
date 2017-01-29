d368680cf8cb291442d944b19ebf60ebcfb532f3
Robert <ambitiousbusinessowner@gmail.com>
2017-01-29 03:11:57 -0500

    hoped to have fixed read()

--- behavior.js
+++ behavior.js
@@ -32,11 +32,9 @@ function read(URL, callback) {
                         if (index > 0) {
                             var scriptTag = document.createElement("script");
                             scriptTag.appendChild(document.createTextNode(script.slice(script.indexOf(">"), script.indexOf("</script>"))));
-                            container.insertBefore(scriptTag, container.getElementsByTagName("script")[index]);
-                            /*
+                            container.insertBefore(container.getElementsByTagName("script")[index], container.getElementsByTagName("script")[index]);
                             var oldTag = container.getElementsByTagName("script")[index+1];
-                            oldTag.parentNode.removeChild(oldTag);  // Old is undefined?
-                            */
+                            oldTag.parentNode.removeChild(oldTag);
                         }
                     });
                 }
