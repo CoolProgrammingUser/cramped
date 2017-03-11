3785763cd10a310da1f6ceb76daa1179798c8d23
Robert <ambitiousbusinessowner@gmail.com>
2017-03-11 14:29:17 -0500

    waited for the page to load to execute stuff

--- behavior.js
+++ behavior.js
@@ -17,32 +17,36 @@ var options = {
 document.head.insertBefore(document.createElement("title"), document.head.childNodes[0]);
 document.getElementsByTagName("title")[0].innerHTML = "Cramped";
 
-// makes a bunch of my faces follow the cursor
-var faceArray = [
-    document.createElement("div"),
-    document.createElement("div"),
-    document.createElement("div"),
-    document.createElement("div"),
-    document.createElement("div")
-    ];
-faceArray.forEach(function(face) {
-    face.style.width = "2em";
-    face.style.height = "2em";
-    face.style.backgroundImage = "url(images/Me in a Nutshell.png)";
-    face.style.backgroundSize = "cover";
-    face.style.position = "absolute";
-    face.style.left = "-20";
-    face.style.top = "-20";
-    document.appendChild(face);
-});
+//holds the cursor position
 var cursor = {"x":"-20", "y":"-20"};
-document.addEventListener("mousemove", function(event) {
-    cursor.x = event.pageX;
-    cursor.y = event.pageY;
-});
-setInterval(function() {
-    faceArray.forEach(function(face, index) {
-        face.style.left = (cursor.x-face.style.left)/(index*10);
-        face.style.top = (cursor.y-face.style.top)/(index*10);
+
+window.addEventListener("load", function() {
+    // makes a bunch of my faces follow the cursor
+    var faceArray = [
+        document.createElement("div"),
+        document.createElement("div"),
+        document.createElement("div"),
+        document.createElement("div"),
+        document.createElement("div")
+        ];
+    faceArray.forEach(function(face) {
+        face.style.width = "2em";
+        face.style.height = "2em";
+        face.style.backgroundImage = "url(images/Me in a Nutshell.png)";
+        face.style.backgroundSize = "cover";
+        face.style.position = "absolute";
+        face.style.left = "-20";
+        face.style.top = "-20";
+        document.appendChild(face);
     });
-}, 20);
+    document.addEventListener("mousemove", function(event) {
+        cursor.x = event.pageX;
+        cursor.y = event.pageY;
+    });
+    setInterval(function() {
+        faceArray.forEach(function(face, index) {
+            face.style.left = (cursor.x-face.style.left)/(index*10);
+            face.style.top = (cursor.y-face.style.top)/(index*10);
+        });
+    }, 20);
+});
